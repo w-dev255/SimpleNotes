2bab8e0400aa7e57fe49ad666ae0394951272c17
w-dev255 <TynIbI@proton.me>
2025-11-15 21:53:10 +0100

    Add files via upload
    A simple utility designed and tested in an android termux environment. Its purpose and facilitate the generation of notes and in the future their management (e.s rename them, move them, cancel them etc... ).

---
+++ SimpleNotes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+creator="w"
+
+code_name="note"
+
+date=$(date +"%Y-%m-%d-%H_%M_%S")
+
+note_name="note_$date.txt"
+
+notes=$(realpath notes)
+
+if [[ -e "$notes" ]]; then
+        sleep 1
+else
+        echo "$code_name : notes not exists."
+        echo "$code_name : Generation in progress..."
+	mkdir -p notes
+fi
+
+
+echo "Software made by $creator"
+echo -e "\n$code_name : Writing a note... "
+while true; do
+read -r -p "note: " str
+if [[ -z "$str" ]]; then
+	echo "$code_name : You should add a text..."
+else
+	echo "$code_name : The string is not empy..."
+	sleep 1
+	break
+fi
+done
+
+while true; do
+	cd "$notes" && touch "$note_name"
+	echo "$str" > "$note_name"
+	echo "$code_name : Note saved as $note_name in $notes/"
+	break 
+done
